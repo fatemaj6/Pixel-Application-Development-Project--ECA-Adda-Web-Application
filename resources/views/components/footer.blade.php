4cec155d5755a325e7fd3b18bf395dc249b69b26
Taqia Tahmida <taqia@graduate.utm.com>
2026-01-01 19:08:32 +0800

    footer fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,48 +1,48 @@
-<footer class="w-full py-16 bg-gray-100 relative z-10">
-    <div class="max-w-3xl mx-auto flex flex-col items-center space-y-6">
+<footer class="w-full py-8 md:py-10 bg-gray-100 relative z-10">
+    <div class="max-w-3xl mx-auto flex flex-col items-center space-y-4">
 
         {{-- Social Icons Row --}}
-        <div class="flex space-x-6">
+        <div class="flex space-x-4">
 
             {{-- Facebook --}}
             <a href="https://www.facebook.com/diproprattoytutorials?_rdc=2&_rdr" target="_blank" 
-               class="w-12 h-12 flex items-center justify-center 
+               class="w-10 h-10 flex items-center justify-center 
                bg-white rounded-full shadow hover:scale-110 transition duration-300">
-                <img src="/landing/images/footer/facebook.png" class="w-6" alt="facebook">
+                <img src="/landing/images/footer/facebook.png" class="w-5" alt="facebook">
             </a>
 
             {{-- Instagram --}}
             <a href="https://www.instagram.com/dp_tutorials/" target="_blank" 
-               class="w-12 h-12 flex items-center justify-center 
+               class="w-10 h-10 flex items-center justify-center 
                bg-white rounded-full shadow hover:scale-110 transition duration-300">
-                <img src="/landing/images/footer/instagram.png" class="w-6" alt="instagram">
+                <img src="/landing/images/footer/instagram.png" class="w-5" alt="instagram">
             </a>
 
             {{-- YouTube --}}
             <a href="https://www.youtube.com/@DiproPrattoy" target="_blank" 
-               class="w-12 h-12 flex items-center justify-center 
+               class="w-10 h-10 flex items-center justify-center 
                bg-white rounded-full shadow hover:scale-110 transition duration-300">
-                <img src="/landing/images/footer/youtube.png" class="w-6" alt="youtube">
+                <img src="/landing/images/footer/youtube.png" class="w-5" alt="youtube">
             </a>
 
             {{-- TikTok --}}
             <a href="https://www.tiktok.com/@dp_tutorials" target="_blank" 
-               class="w-12 h-12 flex items-center justify-center 
+               class="w-10 h-10 flex items-center justify-center 
                bg-white rounded-full shadow hover:scale-110 transition duration-300">
-                <img src="/landing/images/footer/tiktok.png" class="w-6" alt="tiktok">
+                <img src="/landing/images/footer/tiktok.png" class="w-5" alt="tiktok">
             </a>
 
             {{-- LinkedIn --}}
             <a href="https://www.linkedin.com/company/dipro-prattoy-tutorials/?viewAsMember=true" target="_blank" 
-               class="w-12 h-12 flex items-center justify-center 
+               class="w-10 h-10 flex items-center justify-center 
                bg-white rounded-full shadow hover:scale-110 transition duration-300">
-                <img src="/landing/images/footer/linkedin.png" class="w-6" alt="linkedin">
+                <img src="/landing/images/footer/linkedin.png" class="w-5" alt="linkedin">
             </a>
         </div>
 
         {{-- Email --}}
-        <div class="flex items-center space-x-3 text-gray-600 text-lg font-medium">
-            <img src="/landing/images/footer/mail.png" class="w-5" alt="email">
+        <div class="flex items-center space-x-3 text-gray-600 text-base font-medium">
+            <img src="/landing/images/footer/mail.png" class="w-4" alt="email">
 
             <a href="mailto:dptutorials.website@gmail.com"
                class="hover:text-orange-500 transition">
